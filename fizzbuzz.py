59ed05d94341d7fce603a82e0e60cfc267096297
Greg Edens <gregedens@live.com>
2023-10-26 12:44:49 -0400

    I am finished, I believe. Program runs correctly but pytest fails. I don't know why.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,10 @@
 # add your code here
-
+for number in range(1,101):
+    if number % 3 == 0 and number % 4 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 4 == 0:
+        print("Buzz")
+    else:
+        print(str(number))
